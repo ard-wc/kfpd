3841e6ed41d889ce7d2dbeccb1a0a0fe66e650de
ard-wc <99061177+ard-wc@users.noreply.github.com>
2022-02-26 03:13:01 -0600

    Update MPR121_Multiplexer.ino

--- MPR121_Multiplexer.ino
+++ MPR121_Multiplexer.ino
@@ -16,24 +16,24 @@ BSD license, all text above must be included in any redistribution
 **********************************************************/
 
 /***************************************************
- * Code works for two capacitive sensors
+ * Works for six capacitive sensors
  */
-
+// built in library for using I2C devices
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
-
 #ifndef _BV
 #define _BV(bit) (1 << (bit)) 
 #endif
-
 #define MULTIPLEXER_ADDRESS 0x70
 
-void choose_multiplexer_port(uint8_t i) {
-  if (i > 1) return;
- 
+void choose_multiplexer_port(uint8_t i)
+{
+  if (i <= 1)
+  {
   Wire.beginTransmission(MULTIPLEXER_ADDRESS);
   Wire.write(1 << i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
+  }
 }
 
 Adafruit_MPR121 cap = Adafruit_MPR121();
@@ -43,8 +43,6 @@ Adafruit_MPR121 cap4 = Adafruit_MPR121();
 Adafruit_MPR121 cap5 = Adafruit_MPR121();
 Adafruit_MPR121 cap6 = Adafruit_MPR121();
 
-// Keeps track of the last pins touched
-// so we know when buttons are 'released'
 uint16_t last_pin_touched = 0;
 uint16_t current_pin_touched = 0;
 uint16_t last_pin_touched2 = 0;
@@ -58,129 +56,177 @@ uint16_t current_pin_touched5 = 0;
 uint16_t last_pin_touched6 = 0;
 uint16_t current_pin_touched6 = 0;
 
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
 
-  while (!Serial) { 
+  while (!Serial)
+  { 
     // delay keeps Nano from starting too fast
     delay(10);
   } 
 
+  Wire.begin();
+  Serial.begin(9600);  
+
   // Set up the first three sensors on port 0 of multiplexer
   choose_multiplexer_port(0);
-  if (!cap.begin(0x5A)) {
+  if (cap.begin(0x5A))
+  {
+    Serial.println("0x5A Port 0 - touch sensor is working");
+  }
+  else
+  {
     Serial.println("0x5A Port 0 - touch sensor is not connected correctly");
-    while (1);
   }
-  Serial.println("0x5A Port 0 - touch sensor is working");
 
-  if (!cap2.begin(0x5B)) {
+  if (cap2.begin(0x5B))
+  {
+    Serial.println("0x5B Port 0 - touch sensor is working");
+  }
+  else
+  {
     Serial.println("0x5B Port 0 - touch sensor is not connected correctly");
-    while (1);
   }
-  Serial.println("0x5B Port 0 - touch sensor is working");
   
-  if (!cap3.begin(0x5C)) {
-    Serial.println("0x5C Port 0 - touch sensor is not connected correctly");
-    while (1);
+  if (cap3.begin(0x5C))
+  {
+    Serial.println("0x5C Port 0 - touch sensor is working");
   }
-  Serial.println("0x5C Port 0 - touch sensor is working");
+  else
+  {
+    Serial.println("0x5C Port 0 - touch sensor is not connected correctly");
+  }
 
   // Set up the second three sensors on port 0 of multiplexer
   choose_multiplexer_port(1);
-  if (!cap4.begin(0x5A)) {
+  if (cap4.begin(0x5A))
+  {
+    Serial.println("0x5A Port 1 - touch sensor is working");
+  }
+  else
+  {
     Serial.println("0x5A Port 1 - touch sensor is not connected correctly");
-    while (1);
   }
-  Serial.println("0x5A Port 1 - touch sensor is working");
 
-  if (!cap5.begin(0x5B)) {
+  if (cap5.begin(0x5B))
+  {
+    Serial.println("0x5B Port 1 - touch sensor is working");
+  }
+  else
+  {
     Serial.println("0x5B Port 1 - touch sensor is not connected correctly");
-    while (1);
   }
-  Serial.println("0x5B Port 1 - touch sensor is working");
   
-  if (!cap6.begin(0x5C)) {
-    Serial.println("0x5C Port 1 - touch sensor is not connected correctly");
-    while (1);
+  if (cap6.begin(0x5C))
+  {
+    Serial.println("0x5C Port 1 - touch sensor is working");
   }
-  Serial.println("0x5C Port 1 - touch sensor is working");
-
-  // Adjust the sensitivity
-  uint8_t touch_threshold = 12
-  uint8_t release_threshold = 6
-  setThresholds(touch_threshold, release_threshold)
+  else
+  {
+    Serial.println("0x5C Port 1 - touch sensor is not connected correctly");
+  }
 }
 
-void loop() {
+void loop()
+{
   // Get the currently touched pins
+  choose_multiplexer_port(0);
   current_pin_touched = cap.touched();
   current_pin_touched2 = cap2.touched();
   current_pin_touched3 = cap3.touched();
+  choose_multiplexer_port(1);
   current_pin_touched4 = cap4.touched();
   current_pin_touched5 = cap5.touched();
   current_pin_touched6 = cap6.touched();
-  
-  for (uint8_t i=0; i<12; i++) {
+
+  for (uint8_t i=0; i<12; i++)
+  {
     choose_multiplexer_port(0);
-    if ((current_pin_touched & _BV(i)) && !(last_pin_touched & _BV(i)) ) {
-      Serial.print(i); Serial.print(" "); Serial.println(true);
+    // Is touched in current iteration of loop but was not touched in previous iteration
+    if ((current_pin_touched & _BV(i)) && !(last_pin_touched & _BV(i)) )
+    {
+      if (i < 10)
+      {
+        Serial.print(0); Serial.print(i); Serial.print(" "); Serial.println(true);
+      }
+      else
+      {
+        Serial.print(i); Serial.print(" "); Serial.println(true);
+      }
     }
     
-    if (!(current_pin_touched & _BV(i)) && (last_pin_touched & _BV(i)) ) {
-      Serial.print(i); Serial.print(" "); Serial.println(false);
+    // Is not touched in current iteration of loop but was touched in previous iteration
+    if (!(current_pin_touched & _BV(i)) && (last_pin_touched & _BV(i)) )
+    {
+      if (i < 10)
+      {
+        Serial.print(0); Serial.print(i); Serial.print(" "); Serial.println(false);
+      }
+      else
+      {
+        Serial.print(i); Serial.print(" "); Serial.println(false);
+      }
     }
     
-    if ((current_pin_touched2 & _BV(i)) && !(last_pin_touched2 & _BV(i)) ) {
+    if ((current_pin_touched2 & _BV(i)) && !(last_pin_touched2 & _BV(i)) )
+    {
        Serial.print(i+12); Serial.print(" "); Serial.println(true);
     }
     
-    if (!(current_pin_touched2 & _BV(i)) && (last_pin_touched2 & _BV(i)) ) {
+    if (!(current_pin_touched2 & _BV(i)) && (last_pin_touched2 & _BV(i)) )
+    {
       Serial.print(i+12); Serial.print(" "); Serial.println(false);
     }
 
-    if ((current_pin_touched3 & _BV(i)) && !(last_pin_touched3 & _BV(i)) ) {
+    if ((current_pin_touched3 & _BV(i)) && !(last_pin_touched3 & _BV(i)) )
+    {
        Serial.print(i+24); Serial.print(" "); Serial.println(true);
     }
     
-    if (!(current_pin_touched3 & _BV(i)) && (last_pin_touched3 & _BV(i)) ) {
+    if (!(current_pin_touched3 & _BV(i)) && (last_pin_touched3 & _BV(i)) )
+    {
       Serial.print(i+24); Serial.print(" "); Serial.println(false);
     }
 
     choose_multiplexer_port(1);
-    if ((current_pin_touched4 & _BV(i)) && !(last_pin_touched4 & _BV(i)) ) {
+    if ((current_pin_touched4 & _BV(i)) && !(last_pin_touched4 & _BV(i)) )
+    {
       Serial.print(i+36); Serial.print(" "); Serial.println(true);
     }
     
-    if (!(current_pin_touched4 & _BV(i)) && (last_pin_touched4 & _BV(i)) ) {
+    if (!(current_pin_touched4 & _BV(i)) && (last_pin_touched4 & _BV(i)) )
+    {
       Serial.print(i+36); Serial.print(" "); Serial.println(false);
     }
     
-    if ((current_pin_touched5 & _BV(i)) && !(last_pin_touched5 & _BV(i)) ) {
+    if ((current_pin_touched5 & _BV(i)) && !(last_pin_touched5 & _BV(i)) )
+    {
        Serial.print(i+48); Serial.print(" "); Serial.println(true);
     }
     
-    if (!(current_pin_touched5 & _BV(i)) && (last_pin_touched5 & _BV(i)) ) {
+    if (!(current_pin_touched5 & _BV(i)) && (last_pin_touched5 & _BV(i)) )
+    {
       Serial.print(i+48); Serial.print(" "); Serial.println(false);
     }
 
     // we only need to check for one touch node to get to 61 keys
-    if (i == 0 && (current_pin_touched6 & _BV(i)) && !(last_pin_touched6 & _BV(i)) ) {
+    if (i == 0 && (current_pin_touched6 & _BV(i)) && !(last_pin_touched6 & _BV(i)) )
+    {
        Serial.print(i+60); Serial.print(" "); Serial.println(true);
     }
     
-    if (i == 0 && !(current_pin_touched6 & _BV(i)) && (last_pin_touched6 & _BV(i)) ) {
+    if (i == 0 && !(current_pin_touched6 & _BV(i)) && (last_pin_touched6 & _BV(i)) )
+    {
       Serial.print(i+60); Serial.print(" "); Serial.println(false);
     }
   }
 
-  // reset our state
+  choose_multiplexer_port(0);
   last_pin_touched = current_pin_touched;
   last_pin_touched2 = current_pin_touched2;
   last_pin_touched3 = current_pin_touched3;
+  choose_multiplexer_port(1);
   last_pin_touched4 = current_pin_touched4;
   last_pin_touched5 = current_pin_touched5;
   last_pin_touched6 = current_pin_touched6;
-
-  return;
 }
